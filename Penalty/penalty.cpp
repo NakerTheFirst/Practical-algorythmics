617b1a129b69b8eedd0d318ddd0276995e67f220
NakerTheFirst <kcholysk@gmail.com>
2022-04-18 01:42:39 +0200

    Create seperate games counting functions

--- Penalty/penalty.cpp
+++ Penalty/penalty.cpp
@@ -2,11 +2,16 @@
 #include <cmath>
 using std:: cin, std:: cout, std::string, std::endl;
 
+int GS(int T);
+int Y(int G, int A, int D);
+int KS(int G, int A, int Y, int D);
+int X(int gamesGS, int gamesKS, int G);
+
 int main() {
 
     int gamesGS = 0;
     int gamesKS = 0;
-    int G, T, A, D, X, Y, tmp, tmp1;
+    int G, T, A, D, X, Y, x, tmp, tmp1;
 
     // Take input
     cin >> G >> T >> A >> D;
@@ -19,9 +24,9 @@ int main() {
         gamesGS += T-i;
     }
 
+    // Calculate Y
     tmp1 = G*A+D;
-    int x;
-    for (int i = 1; ; ++i) {
+    for (int i = 1;; ++i) {
         x = pow(2, i);
         if (tmp1 == x) {
             Y = 0;
@@ -33,6 +38,7 @@ int main() {
         }
     }
 
+    // Calculate knockout stage games
     tmp = G*A+Y+D;
     while (tmp != 1) {
         tmp /= 2;
@@ -45,4 +51,43 @@ int main() {
     cout << X << "+" << Y;
 
     return 0;
+}
+
+int GS(int T) {
+    int gamesGS = 0;
+    for (int i = 1; T-i > 0; ++i) {
+        gamesGS += T-i;
+    }
+    return gamesGS;
+}
+
+int Y(int G, int A, int D) {
+    int Y;
+    int tmp1 = G*A+D;
+    for (int i = 1;; ++i) {
+        int x = pow(2, i);
+        if (tmp1 == x) {
+            Y = 0;
+            break;
+        }
+        if (tmp1 < x) {
+            Y = pow(2, i) - tmp1;
+            break;
+        }
+    }
+    return Y;
+}
+
+int KS(int G, int A, int Y, int D) {
+    int tmp = G*A+Y+D;
+    int gamesKS = 0;
+    while (tmp != 1) {
+        tmp /= 2;
+        gamesKS += tmp;
+    }
+    return gamesKS;
+}
+
+int X(int gamesGS, int gamesKS, int G) {
+    return gamesGS * G +  gamesKS;
 }
