f3c1db56f8ceac07911ba04218cb7e2941932ff5
NakerTheFirst <kcholysk@gmail.com>
2022-04-18 02:06:31 +0200

    Add comments, clean up the code

--- Penalty/penalty.cpp
+++ Penalty/penalty.cpp
@@ -2,22 +2,18 @@
 #include <cmath>
 using std:: cin, std:: cout, std::string, std::endl;
 
-int GS(int T);
-int Y(int G, int A, int D);
-int KS(int G, int A, int Y, int D);
-int X(int gamesGS, int gamesKS, int G);
-
 int main() {
 
     int gamesGS = 0;
     int gamesKS = 0;
-    int G, T, A, D, X, Y, x, tmp, tmp1;
+    int G, T, A, D, X, Y;
+    int x, temp, temp1;
 
     // Take input
     cin >> G >> T >> A >> D;
 
     // Check for input errors
-    if (G < 1 || T < 1 || A < 1 || D < 1) return 0;
+    if (G < 1 || T < 1 || A < 1 || D < 0) return 0;
 
     // Calculate group stage games
     for (int i = 1; T-i > 0; ++i) {
@@ -25,24 +21,24 @@ int main() {
     }
 
     // Calculate Y
-    tmp1 = G*A+D;
+    temp1 = G*A+D;
     for (int i = 1;; ++i) {
         x = pow(2, i);
-        if (tmp1 == x) {
+        if (temp1 == x) {
             Y = 0;
             break;
         }
-        if (tmp1 < x) {
-            Y = pow(2, i) - tmp1;
+        if (temp1 < x) {
+            Y = pow(2, i) - temp1;
             break;
         }
     }
 
     // Calculate knockout stage games
-    tmp = G*A+Y+D;
-    while (tmp != 1) {
-        tmp /= 2;
-        gamesKS += tmp;
+    temp = G*A+Y+D;
+    while (temp != 1) {
+        temp /= 2;
+        gamesKS += temp;
     }
 
     X = gamesGS * G + gamesKS;
@@ -51,43 +47,4 @@ int main() {
     cout << X << "+" << Y;
 
     return 0;
-}
-
-int GS(int T) {
-    int gamesGS = 0;
-    for (int i = 1; T-i > 0; ++i) {
-        gamesGS += T-i;
-    }
-    return gamesGS;
-}
-
-int Y(int G, int A, int D) {
-    int Y;
-    int tmp1 = G*A+D;
-    for (int i = 1;; ++i) {
-        int x = pow(2, i);
-        if (tmp1 == x) {
-            Y = 0;
-            break;
-        }
-        if (tmp1 < x) {
-            Y = pow(2, i) - tmp1;
-            break;
-        }
-    }
-    return Y;
-}
-
-int KS(int G, int A, int Y, int D) {
-    int tmp = G*A+Y+D;
-    int gamesKS = 0;
-    while (tmp != 1) {
-        tmp /= 2;
-        gamesKS += tmp;
-    }
-    return gamesKS;
-}
-
-int X(int gamesGS, int gamesKS, int G) {
-    return gamesGS * G +  gamesKS;
 }
