45ce070ad4490ed59d24541f2cefc71e627e5b3b
NakerTheFirst <kcholysk@gmail.com>
2022-07-18 22:35:05 +0200

    Add reverse function

--- Adding/adding.cpp
+++ Adding/adding.cpp
@@ -1,46 +1,61 @@
 #include <iostream>
 #include <vector>
-#include <utility>
+#include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-// Utility function - remove trailing zeros
+// Remove trailing zeros
 int handle_trailing_zeros(int num) {
     int num_without_trailing_zeros;
 
-    // code
+    // If the last digit is not 0, return num
 
     return num_without_trailing_zeros;
 }
 
-int reverse(int num) {
-    int reversed_num;
+int reverse_number(int num) {
+    if (num < 10 && num > 0) return num;
 
-    // code
+    int reversed_num = 0;
+    int n, remainder;
+
+    while(num != 0) {
+        remainder = num % 10;
+        reversed_num = reversed_num * 10 + remainder;
+        num /= 10;
+    }
 
     return reversed_num;
 }
 
 int main() {
 
-    int N;
-    int num1, num2;
+    int N, num1, num2, sum, iter = 0;
     std::vector<int> nums;
 
+    // Take number of test cases
     cin >> N;
 
+    // Take numbers to reverse
     for (int i = 0; i < N; ++i) {
         cin >> num1 >> num2;
         nums.push_back(num1);
         nums.push_back(num2);
     }
 
-    // Print the output
-    for (int i = 0; i < 2*N; ++i) {
-        cout << "Pair: " << nums[i] << ", " << nums[i+1] << "\n";
-        ++i;
+    // Overwrite the numbers in nums with their reversed versions
+    for (int i = 0; i < nums.size(); ++i) {
+        nums[i] = reverse_number(nums[i]);
+    }
+
+    // Print the desired output
+    for (int i = 0; i < N; ++i) {
+        sum = nums[iter] + nums[iter+1];
+        cout << reverse_number(sum) << "\n";
+        iter++;
+        iter++;
     }
 
     return 0;
