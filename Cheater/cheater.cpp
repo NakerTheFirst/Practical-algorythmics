721918a0bfce86ad194e9d02e11aca5b4497ce44
NakerTheFirst <kcholysk@gmail.com>
2022-07-30 23:45:10 +0200

    Delegade taking input to separate containers by name
    Add printing utility function as well

--- Cheater/cheater.cpp
+++ Cheater/cheater.cpp
@@ -4,39 +4,64 @@
 using std::cin;
 using std::cout;
 
+// TODO: Convert card names to values
+// TODO:
 
 class Cards {
 public:
     std::vector<char> cardName;
     unsigned int cardValue;
     unsigned int cardSuit;
+
+    // Utility method
+    void printCardsNames(Cards cardsObj) {
+        for (int i = 0; i < cardsObj.cardName.size(); ++i) {
+            if (i % 2 == 0 && i != 0) cout << "\n";
+            cout << cardsObj.cardName[i];
+        }
+    }
 };
 
 
 int main() {
 
-    int N, k;
+    int N, k, iter = 0;
     char cNameByChar;
-    Cards cardsObj;
+    bool close = false;
+    Cards AdamCards;
+    Cards EveCards;
 
-    // Take the number of test cases
+    // Number of test cases
     cin >> N;
 
-    // Take the number of cards per player and check its correction
-    cin >> k;
-    if (k < 0 || k > 26) return 0;
+    // Input taking loop
+    while (!close) {
 
-    // Take card names
-    for (int i = 0; i < k*4; ++i) {
-        cin >> cNameByChar;
-        cardsObj.cardName.push_back(cNameByChar);
+        // Number of cards per player
+        cin >> k;
+        if (k < 0 || k > 26) return 0;
+
+        // Take Adam's cards names
+        for (int i = 0; i < k*2; ++i) {
+            cin >> cNameByChar;
+            AdamCards.cardName.push_back(cNameByChar);
+        }
+
+        // Take Eve's cards names
+        for (int i = k*2; i < k*4; ++i) {
+            cin >> cNameByChar;
+            EveCards.cardName.push_back(cNameByChar);
+        }
+
+        iter++;
+        if (iter == N) close = true;
     }
 
-    // Print output for debugging
-    for (int i = 0; i <= k*2; i+=2) {
-        cout << cardsObj.cardName[i];
-        cout << cardsObj.cardName[i+1] << "\n";
-    }
+    cout << "Adam's cards: \n";
+    AdamCards.printCardsNames(AdamCards);
+
+    cout << "\nEve's cards: \n";
+    EveCards.printCardsNames(EveCards);
 
     return 0;
 }
